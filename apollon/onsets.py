c2ee60c23a07e02b29113650b0e8fc2c121192a2
Michael Blaß <michael.blass@uni-hamburg.de>
2017-08-08 12:47:28 +0200

    A class for onset detection.

--- apollon/onsets.py
+++ apollon/onsets.py
@@ -11,39 +11,56 @@ from apollon import fractal as _fractal
 from apollon import segment as _segment
 
 
-def detect(sig, tau=10, m=3, bins=50, wlen=16, whs=8, order=22):
-    """Detect note onsets in (percussive) music as local maxima of information
-       entropy.
+class OnsetDetector:
 
-    Params:
-        sig      (array-like) Audio signal.
-        tau      (int) Phase-space: delay parameter in samples.
-        m        (int) Phase-space: number of dimensions.
-        bins     (int) Phase-space: number of boxes per axis.
-        wlen     (int) Segmentation: window length in ms.
-        whs      (int) Segmentation: window displacement in ms.
-        order    (int) Peak-picling: Order of filter in samples.
+    __slots__ = ['audio_file_name', 'bins', 'H', 'idx', 'm', 'order',
+                 'tau', 'time_stamp', 'window_length', 'window_hop_size']
 
-    Return:
-        (tuple) array of indices, list of entropy values
-    """
-    # segment audio
-    chunks = _segment.by_ms_with_hop(sig, wlen, whs)
+    def __init__(self):
+        pass
 
-    # calculate entropy for each chunk
-    H = _np.empty(len(chunks))
-    for i, ch in enumerate(chunks):
-        em = _fractal.embedding(ch, tau, m=m, mode='wrap')
-        H[i] = _fractal.pps_entropy(em, bins)
+    def detect(self, sig, tau=10, m=3, bins=50, wlen=16, whs=8, order=22):
+        """Detect note onsets in (percussive) music as local maxima of information
+        entropy.
 
-    # Take imaginary part of the Hilbert transform of the enropy
-    H = _sps.hilbert(H).imag
+        Params:
+            sig      (array-like) Audio signal.
+            tau      (int) Phase-space: delay parameter in samples.
+            m        (int) Phase-space: number of dimensions.
+            bins     (int) Phase-space: number of boxes per axis.
+            wlen     (int) Segmentation: window length in ms.
+            whs      (int) Segmentation: window displacement in ms.
+            order    (int) Peak-picling: Order of filter in samples.
 
-    # pick the peaks
-    w = _np.absolute(H)    # use absolute val to consider negative peaks, too
-    odf = _sps.argrelmax(w, order=order)[0]
+        Return:
+            (tuple) array of indices, list of entropy values
+        """
+        # meta
+        self.audio_file_name = str(sig.file)
+        self.bins = bins
+        self.m = m
+        self.order = order
+        self.tau = tau
+        self.time_stamp = _tools.time_stamp()
+        self.window_hop_size = wlen
+        self.window_length = whs
 
-    # calculate onset position to be in the middle of chunks
-    onsets_idx = [(i+j)//2 for (i, j) in chunks.get_limits()[odf]]
 
-    return onsets_idx, H
+        # segment audio
+        chunks = _segment.by_ms_with_hop(sig, self.window_length, self.window_hop_size)
+
+        # calculate entropy for each chunk
+        self.H = _np.empty(len(chunks))
+        for i, ch in enumerate(chunks):
+            em = _fractal.embedding(ch, self.tau, m=self.m, mode='wrap')
+            self.H[i] = _fractal.pps_entropy(em, self.bins)
+
+        # Take imaginary part of the Hilbert transform of the enropy
+        self.H = _sps.hilbert(self.H).imag
+
+        # pick the peaks
+        odf = _np.absolute(self.H)    # use magnitude to consider negative peaks, too
+        peaks, = _sps.argrelmax(odf, order=self.order)
+
+        # calculate onset position to be in the middle of chunks
+        self.idx = [(i+j)//2 for (i, j) in chunks.get_limits()[peaks]]
