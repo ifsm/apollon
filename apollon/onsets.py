c682d769064c4a74b524cf2c5189305e91d7b6ad
Michael Blaß <michael.blass@uni-hamburg.de>
2017-08-09 13:46:10 +0200

    Fixed onsets

--- apollon/onsets.py
+++ apollon/onsets.py
@@ -8,12 +8,13 @@ import scipy.signal as _sps
 
 from apollon import fractal as _fractal
 from apollon import segment as _segment
+from apollon import tools as _tools
 
 
 class OnsetDetector:
 
-    __slots__ = ['audio_file_name', 'bins', 'H', 'idx', 'm', 'order',
-                 'tau', 'time_stamp', 'window_length', 'window_hop_size']
+    __slots__ = ['audio_file_name', 'bins', 'idx', 'm', 'odf', 'order', 'tau',
+                 'time_stamp', 'window_length', 'window_hop_size']
 
     def __init__(self):
         pass
@@ -41,25 +42,25 @@ class OnsetDetector:
         self.order = order
         self.tau = tau
         self.time_stamp = _tools.time_stamp()
-        self.window_hop_size = wlen
-        self.window_length = whs
+        self.window_hop_size = whs
+        self.window_length = wlen
 
 
         # segment audio
-        chunks = _segment.by_ms_with_hop(sig, self.window_length, self.window_hop_size)
+        chunks = _segment.by_ms_with_hop(sig, self.window_length,
+                                         self.window_hop_size)
 
         # calculate entropy for each chunk
-        self.H = _np.empty(len(chunks))
+        H = _np.empty(len(chunks))
         for i, ch in enumerate(chunks):
             em = _fractal.embedding(ch, self.tau, m=self.m, mode='wrap')
-            self.H[i] = _fractal.pps_entropy(em, self.bins)
+            H[i] = _fractal.pps_entropy(em, self.bins)
 
         # Take imaginary part of the Hilbert transform of the enropy
-        self.H = _sps.hilbert(self.H).imag
+        self.odf = _np.absolute(_sps.hilbert(H).imag)
 
         # pick the peaks
-        odf = _np.absolute(self.H)    # use magnitude to consider negative peaks, too
-        peaks, = _sps.argrelmax(odf, order=self.order)
+        peaks, = _sps.argrelmax(self.odf, order=self.order)
 
         # calculate onset position to be in the middle of chunks
         self.idx = [(i+j)//2 for (i, j) in chunks.get_limits()[peaks]]
