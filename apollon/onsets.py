2837ee90dcfcb8fe4d1af6b76587d42f3d47ec99
fknv184 <fknv184@uni-hamburg.de>
2017-11-03 11:51:10 +0100

    Add FluxOnsetDetector

--- apollon/onsets.py
+++ apollon/onsets.py
@@ -5,13 +5,15 @@
 import matplotlib.pyplot as _plt
 import numpy as _np
 import scipy.signal as _sps
+import mir_eval as _me
 
 from apollon import fractal as _fractal
 from apollon import segment as _segment
 from apollon import tools as _tools
+from apollon.signal.spectral import STFT
 
 
-class OnsetDetector:
+class EnrtopyOnsetDetector:
 
     __slots__ = ['audio_file_name', 'bins', 'idx', 'm', 'odf', 'order', 'tau',
                  'time_stamp', 'window_length', 'window_hop_size']
@@ -62,3 +64,79 @@ class OnsetDetector:
         # calculate onset position to be in the middle of chunks
         self.idx = _np.array( [(i+j)//2
                               for (i, j) in chunks.get_limits()[peaks]])
+
+
+def peak_picking(odf, w=3, m=3, alpha=.1, delta=.1):
+
+    g = [0]
+    out = []
+
+    for n, val in enumerate(odf):
+
+        # set local window
+        idx = _np.arange(n-m*w, n+w, 1)
+        window = _np.take(odf, idx, mode='clip')
+
+        # check three onset conditions
+        #
+
+        # First condition: val must be the biggest value within the local window
+        cond1 = _np.all(val >= window)
+
+        # Second condition: val must be bigger then the local windows's mean plus delta
+        cond2 = val >= (_np.mean(window) + delta)
+
+        # Third condition: val must be bigger then an adaptive threshold g(n, alpha)
+        foo = max(val, alpha*g[n] + (1-alpha)*val)    # ATTENTION: This MUST be built-in max()
+        g.append(foo)
+        cond3 = val >= foo
+
+        if cond1 and cond2 and cond3:
+            out.append(n)
+
+    return _np.array(out)
+
+
+class FluxOnsetDetector:
+    def __init__(self, sig, sr, nseg=2048, hop=441):
+
+        X = STFT(sig, sr, nseg=nseg, nover=nseg-hop)
+        #dX = np.gradient(X.abs(), 1/hop, axis=1)
+        dX = _np.diff(X.abs())
+
+        odf = L1_Norm(np.maximum(dX, 0, dX))
+
+        wh = np.hamming(10)
+        odf = convolve(odf, wh, mode='same')
+
+        self.odf = ztrans(odf)
+
+        self.peaks = peak_picking(odf)
+
+        self.times = self.peaks * hop / sr
+
+
+
+def evaluate_onsets(targets:   Dict[str, np.ndarray],
+                    estimates: Dict[str, np.ndarray]) -> Tuple[float, float, float]:
+    """Evaluate the performance of an onset detection.
+
+    Params:
+        targets    (dict) of ground truth onset times, with
+                            keys   == file names, and
+                            values == target onset times in ms.
+
+        estimates  (dict) of estimated onsets times, with
+                            keys   == file names, and
+                            values == estimated onset times in ms.
+
+    Return:
+        (p, r, f)    Tupel of precison, recall, f-measure
+    """
+
+    out = []
+    for name, tvals in targets.items():
+        od_eval = _me.onset.evaluate(tvals, estimates[name])
+        out.append([i for i in od_eval.values()])
+
+    return _np.array(out)
