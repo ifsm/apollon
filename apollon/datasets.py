f589e67622fa4de759e8494499c42d98c5f09df9
fknv184 <michael.blass@uni-hamburg.de>
2017-11-13 12:02:18 +0100

    add datasets

---
+++ apollon/datasets.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple as _namedtuple
+
+import numpy as _np
+
+from apollon.__init__ import APOLLON_PATH
+
+
+def load_earthquakes() -> _namedtuple:
+    """Load earthquakes dataset.
+       Return:
+            (namedtuple) EqData(data, N, descr)
+    """
+
+    # set file paths
+    eq_data_path = APOLLON_PATH + '//datasets/earthquakes.data'
+    eq_descr_path = APOLLON_PATH + '//datasets/earthquakes.md'
+
+    # load data
+    data = _np.fromfile(eq_data_path, dtype='uint8', sep=',')
+    N = len(data)
+
+    with open(eq_descr_path) as fobj:
+        descr = ''.join(row for row in fobj)
+
+    # construct dataset object
+    EqData = _namedtuple('EarthquakeData', ('data', 'N', 'description'))
+    eqd = EqData(data, len(data), descr)
+
+    return eqd
---
+++ apollon/datasets/earthquakes.md
@@ -0,0 +1,24 @@
+Earthquakes data set
+====================
+
+
+Description
+------------
+
+Number of earthquakes with magnitude greater than or equal to 7 happening
+around the globe between 1900 and 2006.
+
+
+Credits
+-------
+
+Data was transcribed from:
+
+    [1] W. Zucchini & I. L. MacDonald,
+        *Hidden Markov Models for Time Series*,
+        (Monographs on Statistics and Applied Probability 110),
+        New York: Chapman & Hall, 2009, p. 4.
+
+The data was originally recorded by the U.S. Geological Survey and downloaded
+from [http://neic.usgs.gov/neis/eqlists](http://neic.usgs.gov/neis/eqlists) on
+25th of July 2007 by [1]. Unfortunately the link has expired.
