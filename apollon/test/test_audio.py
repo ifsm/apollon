2f7cbb3cc3697afe39083414d49ab168c76bb158
Michael Blaß <michael.blass@uni-hamburg.de>
2018-04-04 10:50:17 +0200

    add test case for audio modul

---
+++ apollon/test/test_audio.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import numpy as _np
+
+from apollon.signal.audio import loadwav
+
+
+class Test_ModulAudio(unittest.TestCase):
+    def setUp(self):
+        self.x = loadwav('/Users/michael/audio/beat.wav')
+
+    def test_AudioDataAttributes(self):
+        c = isinstance(self.x.fs, int)
+        self.assertTrue(c)
+
+        c = isinstance(self.x.data, _np.ndarray)
+        self.assertTrue(c)
+
+if __name__ == '__main__':
+    unittest.main()
