75f48439aa577e8cf71772038eb46ce371a68840
Michael Blaß <michael.blass@uni-hamburg.de>
2018-08-26 08:59:29 +0200

    EM...

--- apollon/hmm/em.py
+++ apollon/hmm/em.py
@@ -3,70 +3,65 @@
 
 """em.py
 
-Generic implementation of the Expectation Maximization algorithm.
+Expectation Maximization algorithm for HMMs with
+Poisson distributed tate-depended variables.
 """
 
 import numpy as _np
-from scipy import stats
-from apollon.hmm.fwbw import forward_backward
+from scipy import stats as _stats
+from scipy.special import logsumexp as _logsumexp
 
-__author__ = "Michael Blaß"
+from fwbw import forward_backward
 
 
 def EM(x, m, theta, maxiter=1000, tol=1e-6):
-    '''    Estimate the parameters of an m-state PoissonHMM.
+    """Estimate the parameters of an m-state PoissonHMM.
 
     Params:
-        x          (array-like of ints) the data to train the HMM
-        phmm       (int)
-        maxiter    (int) maximum number of EM iterations
-            tol    (float) break the loop if the difference between
-                           to consecutive iterations is < tol
-    '''
-    
+        x           (np.ndarray)    One-dimensional array of integer values.
+        theta       (tuple)         Initial guesses (lambda, gamma, delta).
+        maxiter     (int)           Mmaximum number of EM iterations.
+        tol         (float)         Convergence criterion.
+    """
     n = len(x)
 
-    m_lambda = theta[0].copy()
-    m_gamma = theta[1].copy()
-    m_delta = theta[2].copy()
+    this_lambda = theta[0].copy()
+    this_gamma = theta[1].copy()
+    this_delta = theta[2].copy()
 
     next_lambda = theta[0].copy()
     next_gamma = theta[1].copy()
     next_delta = theta[2].copy()
 
     for i in range(maxiter):
-        alpha, beta, allprobs = forward_backward(x, m, m_lambda, m_gamma, m_delta)
+        alpha, beta, prob = forward_backward(x, m, this_lambda, this_gamma, this_delta)
 
         c = max(alpha[-1])
-        log_likelihood = c + _np.log(_np.sum(_np.exp(alpha[-1] - c)))
+        log_likelihood = c + _logsumexp(alpha[-1] - c)
 
         for j in range(m):
             for k in range(m):
-                next_gamma[j, k] *= _np.sum(_np.exp(alpha[:n - 1, j] +
+                next_gamma[j, k] *= _np.sum(_np.exp(alpha[:n-1, j] +
                                             beta[1:n, k] +
-                                            allprobs[1:n, k] -
+                                            prob[1:n, k] -
                                             log_likelihood))
-
-            rab = _np.exp(alpha[:, j] + beta[:, j] - log_likelihood)
-            next_lambda[j] = _np.sum(rab * x) / _np.sum(rab)
-
         next_gamma /= _np.sum(next_gamma, axis=1)
-        next_delta = _np.exp(alpha[0] + beta[0] - log_likelihood)
-        next_delta /= _np.sum(next_delta)
 
-        crit = (_np.sum(_np.absolute(m_lambda - next_lambda)) +
-                _np.sum(_np.absolute(m_gamma - next_gamma)) +
-                _np.sum(_np.absolute(m_delta - next_delta)))
+        rab = _np.exp(alpha + beta - log_likelihood)
+        next_lambda = (rab * x[:, None]).sum(axis=0) / rab.sum(axis=0)
+
+        next_delta = rab[0] / rab[0].sum()
+
+        crit = (_np.abs(this_lambda - next_lambda).sum() +
+                _np.abs(this_gamma - next_gamma).sum()  +
+                _np.abs(this_delta - next_delta).sum())
 
         if crit < tol:
-            nparams = m*m + m-1
-            aic = -2 * (log_likelihood - nparams)
-            bic = -2 * log_likelihood + nparams * _np.log(n)
+            return this_lambda, this_gamma, this_delta, i, log_likelihood
 
-            return (m_lambda, m_gamma, m_delta, i, -log_likelihood,
-                    0, True, aic, bic)
+        this_lambda = next_lambda.copy()
+        this_gamma = next_gamma.copy()
+        this_delta = next_delta.copy()
 
-        m_lambda = next_lambda
-        m_gamma = next_gamma
-        m_delta = next_delta
     return False
+
--- apollon/hmm/fwbw.py
+++ apollon/hmm/fwbw.py
@@ -5,49 +5,96 @@
 Implementation of the forward backward algorithm."""
 
 
-import numpy as np
-from scipy import stats as stats
+import numpy as _np
+from scipy import stats as _stats
 
 
 # CORRECT 20.06.2016
-def forward_backward(x, m, _lambda, _gamma, _delta):
+def forward_backward1(x, m, _lambda, _gamma, _delta):
     """Fails if _delta has zeros."""
     n = len(x)
-    alpha, beta = np.zeros((2, n, m))
+    alpha, beta = _np.zeros((2, n, m))
+
+    # init forward
+    pprob = _stats.poisson.pmf(x[:, None], _lambda)
+    a_0 = _delta * pprob[0]
+
+    # normalize
+    sum_a = a_0.sum()
+    a_t = a_0 / sum_a
+
+    # scale factor in.log domain
+    lscale = _np.log(sum_a)
+
+    # set first forward prob
+    print(a_t)
+    alpha[0] = _np.log(a_t) + lscale
+
+    # start recursion
+    for i in range(1, n):
+        a_t = a_t @ _gamma * pprob[i]
+        sum_a = a_t.sum()
+        a_t /= sum_a
+        lscale += _np.log(sum_a)
+        alpha[i] = _np.log(a_t) + lscale
+
+    # init backward
+    beta[-1] = 0
+    b_t = _np.repeat(1/m, m)
+    lscale = _np.log(m)
+
+    # start backward recursion
+    for i in range(n-1, 0, -1):
+        b_t = _gamma @ (pprob[i] * b_t)
+        beta[i-1] = _np.log(b_t) + lscale
+        sum_b = b_t.sum()
+        b_t /= sum_b
+        lscale += _np.log(sum_b)
+
+    return alpha, beta, _np.log(pprob)
+
+
+def forward_backward2(x, m, _lambda, _gamma, _delta):
+    """Fails if _delta has zeros."""
+    n = len(x)
+    alpha, beta = _np.zeros((2, n, m))
     
     # init forward
-    allprobs = stats.poisson.pmf(x[:, None], _lambda)
+    allprobs = _stats.poisson.pmf(x[:, None], _lambda)
     a_0 = _delta * allprobs[0]
+
 
     # normalize
     sum_a = a_0.sum()
     a_t = a_0 / sum_a
 
     # scale factor in log domain
-    lscale = np.log(sum_a)
+    lscale = _np.log(sum_a)
 
     # set first forward prob
-    alpha[0] = np.log(a_t) + lscale
-    
+
+    alpha[0] = _np.log(a_t) + lscale
+
     # start recursion
     for i in range(1, n):
         a_t = a_t @ _gamma * allprobs[i]
         sum_a = a_t.sum()
         a_t /= sum_a
-        lscale += np.log(sum_a)
-        alpha[i] = np.log(a_t) + lscale
+        lscale += _np.log(sum_a)
+        alpha[i] = _np.log(a_t) + lscale
         
     # init backward
     beta[-1] = 0
-    b_t = np.repeat(1/m, m)
-    lscale = np.log(m)
-    
+    b_t = _np.repeat(1/m, m)
+    lscale = _np.log(m)
+
     # start backward recursion
-    for i in range(n-2, -1, -1):    # ugly reverse iteration in python
-        b_t = _gamma @ (allprobs[i+1] * b_t)
-        beta[i] = np.log(b_t) + lscale
+    for i in range(n-1, 0, -1):    # ugly reverse iteration in python
+        b_t = _gamma @ (allprobs[i] * b_t)
+        beta[i-1] = _np.log(b_t) + lscale
         sum_b = b_t.sum()
         b_t /= sum_b
-        lscale += np.log(sum_b)
-        
-    return alpha, beta, allprobs
+        lscale += _np.log(sum_b)
+
+    return alpha, beta, _np.log(allprobs)
+    
