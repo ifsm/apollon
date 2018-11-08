021e2b2c6ca17ab32ee5a1ca9d80aabec27bf2d8
Michael Blaß <michael.blass@uni-hamburg.de>
2018-11-08 16:38:15 +0100

    add global var for version number

--- apollon/__init__.py
+++ apollon/__init__.py
@@ -1,4 +1,8 @@
-import os as _os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+import os as _os
+from . _version import __version__
 
 APOLLON_PATH = _os.path.dirname(_os.path.realpath(__file__))
+
