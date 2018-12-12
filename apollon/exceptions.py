bc5601f6f1d8078f13c6bd0a2b7be5181d00bd53
Michael Blaß <michael.blass@uni-hamburg.de>
2018-12-12 17:12:56 +0100

    Refactor

--- apollon/exceptions.py
+++ apollon/exceptions.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+"""
+exceptions.py -- Apollon exceptions.
+Copyright (C) 2018  Michael Blaß <michael.blass@uni-hamburg.de>
 
-__author__ = 'Michael Blaß'
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 class NotAudioDataError(Exception):
-    '''Raised if a wrong data format is loaded by accident.
-    '''
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
-
-
-class ModelFailedError(Exception):
-    '''Raised if HMM training failed.'''
-    def __init__(self, value=None):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
+    """Raised if non-audio data is load."""
