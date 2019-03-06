c4e72c4e4a63728d928ae67e66a63ff93ee67541
Michael Blaß <michael.blass@uni-hamburg.de>
2019-03-06 11:34:33 +0100

    Changed plot params defaults

--- apollon/_defaults.py
+++ apollon/_defaults.py
@@ -13,18 +13,18 @@ lambda_guess_methods = ['quantile', 'linear']
 class plot_params:
 		signal = {'linewidth':1,
 				  'linestyle':'solid',
-				  'color':'b',
-				  'alpha':.7,
-				  'zorder':0}
-
-		sig_ons = {'linewidth':1,
-				  'linestyle':'solid',
 				  'color':'k',
-				  'alpha':.3,
+				  'alpha':.5,
 				  'zorder':0}
 
-		onset = {'linewidth':2,
+		sig_ons = {'linewidth':2,
+				  'linestyle':'solid',
+				  'color':'C1',
+				  'alpha':.9,
+				  'zorder':0}
+
+		onset = {'linewidth':3,
 				 'linestyle':'dashed',
-				 'color':'r',
+				 'color':'C1',
 				 'alpha':.9,
 				 'zorder':10}
