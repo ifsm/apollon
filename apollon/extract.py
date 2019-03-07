d906fb4ae3e6dd9e17206848955984c7305db713
Michael Blaß <michael.blass@uni-hamburg.de>
2019-03-07 16:14:41 +0100

    Refactoring.

--- apollon/extract.py
+++ apollon/extract.py
@@ -3,8 +3,7 @@ Copyright 2019, Michael Blaß
 michael.blass@uni-hamburg.de
 """
 
-import math as _math
-from collections import OrderedDict
+from collections import OrderedDict as _OrderedDict
 
 import numpy as _np
 
@@ -12,41 +11,43 @@ from apollon.signal.spectral import fft as _fft
 from . types import Array as _Array
 
 
-def maxamp(chunk, **kwargs):
-    '''Extract maximal amplitude from chunk.'''
-    return _np.max(_np.absolute(chunk))
+def maxamp(sig: _Array) -> float:
+    """Extract maximal amplitude from chunk."""
+    return _np.max(_np.absolute(sig))
 
 
-def meanamp(chunk, **kwargs):
-    '''Extract mean amplitude from chunk.'''
-    return _np.sum(chunk) / len(chunk)
+def meanamp(sig: _Array) -> float:
+    """Extract mean amplitude from chunk."""
+    return _np.sum(sig) / len(sig)
 
 
-def stdamp(chunk, **kwargs):
-    '''Extract standard deviation from chunk.'''
-    return _np.std(chunk)
+def stdamp(sig: _Array) -> float:
+    """Extract standard deviation from chunk."""
+    return _np.std(sig)
 
 
-def energy(chunk, **kwargs):
-    '''Extract linear energy from chunk.'''
-    return _np.sum(_np.square(chunk))
+def energy(sig: _Array) -> float:
+    """Extract linear energy from chunk."""
+    return _np.sum(_np.square(sig))
 
 
-def entropy(chunk, theta=2, bins=10, window=None, **kwargs):
-    '''Shannon Entropy in [nat], normalized by log(N)'''
+def entropy(sig: _Array, theta: int = 2, bins: int = 10,
+            window: str = 'None') -> float:
+    """Shannon Entropy in [nat], normalized by log(N).
+    """
     if window:
-        pps = PseudoPhaseSpace(chunk * window(len(chunk)), theta, bins)
+        pps = PseudoPhaseSpace(sig * window(len(sig)), theta, bins)
     else:
-        pps = PseudoPhaseSpace(chunk, theta, bins)
+        pps = PseudoPhaseSpace(sig, theta, bins)
     return pps.get_entropy()
 
 
-def rms_energy(chunk, **kwargs):
+def rms_energy(sig: _Array) -> float:
     """Extract rms energy from chunk."""
-    return _math.sqrt(_np.sum(_np.square(chunk)) / len(chunk))
+    return _np.sqrt(_np.sum(_np.square(sig)) / len(sig))
 
 
-def spectral_centroid(inp: _Array, fs: int, window: str = 'hamming') -> _Array:
+def spectral_centroid(sig: _Array, fs: int, window: str = 'hamming') -> _Array:
     """Extract spectral centroid from input array.
 
     Params:
@@ -55,25 +56,25 @@ def spectral_centroid(inp: _Array, fs: int, window: str = 'hamming') -> _Array:
 
     return    (numpy ndarray) of spc values.
     """
-    if inp.ndim == 2:
-        out = _np.zeros(inp.shape[0])
+    if sig.ndim == 2:
+        out = _np.zeros(sig.shape[0])
 
-        for i, seg in enumerate(inp):
+        for i, seg in enumerate(sig):
             out[i] = _fft(seg, fs=fs, window=window).centroid()
         return out
 
-    elif inp.ndim == 1:
-        return _fft(inp, fs=fs, window=window).centroid()
+    elif sig.ndim == 1:
+        return _fft(sig, fs=fs, window=window).centroid()
 
     else:
         raise ValueError('Input array must have at max two dimensions.')
 
 
-available_features = OrderedDict({'maxamp': maxamp,
-                                  'meanamp': meanamp,
-                                  'stdamp': stdamp,
-                                  'entropy': entropy,
-                                  'energy': energy,
-                                  'rms': rms_energy,
-                                  'spc': spectral_centroid,
-                                  })
+available_features = _OrderedDict({'maxamp': maxamp,
+                                   'meanamp': meanamp,
+                                   'stdamp': stdamp,
+                                   'entropy': entropy,
+                                   'energy': energy,
+                                   'rms': rms_energy,
+                                   'spc': spectral_centroid,
+                                   })
