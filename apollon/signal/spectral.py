2d429d888f78dde0727cd469eb7dd6560e2aec8e
Michael Blaß <michael.blass@uni-hamburg.de>
2019-03-14 09:41:44 +0100

    Rewrite stft and Spectrogram.

--- apollon/signal/spectral.py
+++ apollon/signal/spectral.py
@@ -19,11 +19,12 @@ __author__ = 'Michael Blaß'
 
 import numpy as _np
 import matplotlib.pyplot as _plt
-from scipy.signal import stft as _stft
 from scipy.signal import get_window as _get_window
+from deprecation import deprecated as _deprecated
 
-from apollon.signal.tools import amp2db
-
+from apollon.signal.tools import amp2db as _amp2db
+from .. types import Array as _Array
+from apollon import __version__
 
 class _Spectrum_Base:
     def __abs__(self):
@@ -166,7 +167,7 @@ class _Spectrum(_Spectrum_Base):
         if not _plt.isinteractive():
             fig.show()
 
-
+@_deprecated('0.1.3', '0.2', __version__, 'This function is faulty and will be rewritten shortly')
 def fft(signal, fs=None, n=None, window=None):
     """Return the discrete fourier transform of the input.
 
@@ -203,42 +204,56 @@ def fft(signal, fs=None, n=None, window=None):
     return _Spectrum(bins, fs, n, window)
 
 
-class STFT:
-    def __init__(self, sig, sr:int, window:str ='hamming', n_perseg:int=256,
-                       n_overlap:int=None, n_fft:int=None):
-        """Short time Fourier Transform of one-dimensional input `sig`.
+class Spectrogram:
+    def __init__(self, inp:_Array, fs:int, window_name:str, n_perseg:int, hop_size:int) -> None:
+        """Compute a spectrogram of the input data.
 
-        Params:
-            sr          Sample rate.
-            window      Window function name.
-            n_perseg    Number of samples per window.
-            n_overlap   Number of overlaping samples.
-            n_fft       FFT length.
+        Args:
+            inp      (np.ndarray)    Input signal.
+            fs       (int)           Sampling frequency of input signal.
+            window   (str)           Name of window function.
+            n_perseg (int)           Number of samples per DFT.
+            hop_size (int)           Number of samples to shift the window.
+
+        Returns:
+            (Spectrogram)
         """
-        res = _stft(sig, fs=sr, window=window, nperseg=n_perseg,
-                   noverlap=n_overlap, nfft=n_fft)
+        self.inp_size = inp.size
+        self.fs = fs
+        self.window_name = window_name
+        self.n_perseg = n_perseg
+        self.hop_size = hop_size
+        self.n_overlap  = self.n_perseg - self.hop_size
 
-        self.freqs, self.t, self.bins = res
+        self.bins = None
+        self.frqs = None
+        self.times = None
+        self._compute_spectrogram(inp)
+
+
+    def _compute_spectrogram(self, inp):
+        shp_x = (self.inp_size - self.n_overlap ) // self.hop_size
+        shp_y = self.n_perseg
+
+        strd_x = self.hop_size * inp.strides[0]
+        strd_y = inp.strides[0]
+
+        inp_strided = _np.lib.stride_tricks.as_strided(inp, (shp_x, shp_y), (strd_x, strd_y))
+        inp_strided = _get_window(self.window_name, shp_y) * inp_strided
+
+        self.bins = _np.fft.rfft(inp_strided) / self.n_perseg
+
+        if shp_y % 2 != 0:
+            self.bins[:, -1] *= 2
         self.bins *= 2
-        self.shape = self.bins.shape
 
+        self.bins = _np.transpose(self.bins)
+        self.frqs = _np.fft.rfftfreq(self.n_perseg, 1/self.fs)
 
-    def plot(self, power:bool = True):
-        """Plot the spectrogram of the STFT.
-        
-        Params:
-            power   Plot power spectrum if True.
-        """
-        if power:
-            XX = self.power()
-        else:
-            XX = self.abs()
+        t_start = shp_y / 2
+        t_stop = inp.size - shp_y / 2 + 1
+        self.times = _np.arange(t_start, t_stop, self.hop_size) / float(self.fs)
 
-        # TODO: implement aplot integration
-        fig, ax = _plt.subplots(1, figsize=(8, 4))
-        ax.pcolormesh(self.t, self.freqs, XX,
-                      cmap='nipy_spectral')
-        return fig, ax
 
     def abs(self):
         return self.__abs__()
@@ -247,11 +262,52 @@ class STFT:
         return _np.square(self.__abs__())
 
     def centroid(self):
-        Xp = self.power()
-        return _np.sum(Xp.T * self.freqs, axis=1) / _np.sum(Xp, axis=0)
+        X_pow = _np.maximum(self.power(), _np.finfo('float64').eps)
+        return _np.sum(X_pow.T * self.frqs, axis=1) / X_pow.sum(axis=0)
 
     def flux(self, hr=True):
         return _np.maximum(_np.diff(self.abs(), axis=1), 0)
 
+    def plot(self, cmap:str = 'nipy_spectral', cbar:bool = True,
+             figsize:tuple = (8, 4)) -> tuple:
+        """Plot the spectrogram in dB scaling.
+
+        Args:
+            cmap    (str)      Colormarp name.
+            cbar    (bool)     Display a color scale if True.
+            figsize (tuple)    Width and height of figure.
+
+        Returns:
+            Tuple    (fig, ax)
+        """
+        fig, ax = _plt.subplots(1, figsize=figsize)
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Frequency [Hz]')
+        cmesh = ax.pcolormesh(self.times, self.frqs, _amp2db(self.abs()), cmap=cmap)
+
+        if cbar:
+            cb = fig.colorbar(cmesh, ax=ax)
+            cb.set_label('db SPL')
+
+        return fig, ax
+
     def __abs__(self):
         return _np.absolute(self.bins)
+
+
+def stft(inp: _Array, fs:int, window:str = 'hanning', n_perseg:int = 512, hop_size:int = 256):
+    """Perform Short Time Fourier Transformation of `inp`
+
+    `inp` is assumed to be an one-dimensional array of real values.
+
+    Args:
+        inp      (np.ndarray)    Input signal.
+        fs       (int)           Sampling frequency of input signal.
+        window   (str)           Name of window function.
+        n_perseg (int)           Number of samples per DFT.
+        hop_size (int)           Number of samples to shift the window.
+
+    Returns:
+        (Spectrogram)
+    """
+    return Spectrogram(inp, fs, window, n_perseg, hop_size)
