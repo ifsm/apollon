8e65a179bd1185b30cd2a2df4ada5c876e469a31
Michael Blaß <michael.blass@uni-hamburg.de>
2019-03-18 18:41:35 +0100

    Add psycho_features extension.

---
+++ apollon/signal/psycho_features_module.c
@@ -0,0 +1,103 @@
+#define NPY_NO_DEPRECATED_API NPY_1_8_API_VERSION
+
+#include <Python.h>
+#include <numpy/arrayobject.h>
+#include <math.h>
+
+
+double _roughness (double d_frq, double amp_1, double amp_2);
+
+
+static PyObject *
+ext_roughness (PyObject *self, PyObject *args)
+{
+    PyObject *py_spectrogram = NULL;
+    PyObject *py_frqs        = NULL;
+
+    PyArrayObject *spctrgrm = NULL;
+    PyArrayObject *frqs     = NULL;
+    PyArrayObject *rghnss   = NULL;
+    npy_intp      *shape    = NULL;
+
+    if (!PyArg_ParseTuple (args, "OO", &py_spectrogram, &py_frqs))
+    {
+        return NULL;
+    }
+
+    spctrgrm = (PyArrayObject *) PyArray_FROM_OTF (py_spectrogram, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    frqs     = (PyArrayObject *) PyArray_FROM_OTF (py_frqs,        NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+    if (spctrgrm == NULL)
+    {
+        PyErr_SetString (PyExc_MemoryError, "Could not allocate spectrogram buffer.\n");
+        Py_RETURN_NONE;
+    }
+
+    shape = PyArray_SHAPE(spctrgrm);
+    rghnss = (PyArrayObject *) PyArray_NewFromDescr
+                (&PyArray_Type, PyArray_DescrFromType (NPY_DOUBLE), 1,
+                 &shape[1], NULL, NULL, 0, NULL);
+
+    if (rghnss == NULL)
+    {
+        PyErr_SetString (PyExc_MemoryError, "Could not allocate spectrogram buffer.\n");
+        Py_RETURN_NONE;
+    }
+
+    double *amp_data = PyArray_DATA (spctrgrm);
+    double *frq_data = PyArray_DATA (frqs);
+    double *r_data   = PyArray_DATA (rghnss);
+    size_t  n_times  = (size_t) shape[0];
+    size_t  n_frqs   = (size_t) shape[1];
+    for (size_t t = 0; t < n_times; t++)
+    {
+        r_data[t] = 0.0f;
+
+        for (size_t i = 0; i < n_frqs - 1; i++)
+        {
+            for (size_t j = i+1; j < n_frqs; j++)
+            {
+                double d_frq = fabs (frq_data[i] - frq_data[j]);
+                if (d_frq >= 300.0f)
+                {
+                    break;
+                }
+                r_data[t] += _roughness (d_frq, amp_data[i*n_frqs+t], amp_data[j*n_frqs+t]);
+            }
+        }
+    }
+    return (PyObject *) rghnss;
+}
+
+
+double _roughness (double d_frq, double amp_1, double amp_2)
+{
+    double frq_rmax = 33.0f;
+    return amp_1 * amp_2 * (d_frq / (frq_rmax * exp (-1.0))) * exp (-d_frq/frq_rmax);
+}
+
+
+static PyMethodDef
+PF_Methods[] = {
+    {"roughness", ext_roughness, METH_VARARGS,
+     "roughness(spctrgrm, frqs)"},
+
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef
+psycho_features_module = {
+    PyModuleDef_HEAD_INIT,
+    "psycho_features",
+    NULL,
+    -1,
+    PF_Methods
+};
+
+PyMODINIT_FUNC
+PyInit_psycho_features (void)
+{
+    import_array ();
+    return PyModule_Create (&psycho_features_module);
+}
+
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #! python3
 
-from setuptools import setup
+from setuptools import setup, Extension
 from setuptools.config import read_configuration
+from numpy.distutils.misc_util import get_numpy_include_dirs
+
 
 config = read_configuration('./setup.cfg')
 
-setup()
+psycho_features = Extension('apollon.signal.psycho_features', sources=['apollon/signal/psycho_features_module.c'])
+
+setup(include_dirs =  get_numpy_include_dirs(),
+      ext_modules  = [psycho_features])
