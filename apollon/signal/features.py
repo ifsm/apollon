78a8780e2bc7c2d558227b4bee1caa7182fa8891
Michael Blaß <michael.blass@uni-hamburg.de>
2019-03-20 18:05:17 +0100

    Add spectral_centroid and spectral_flux

--- apollon/signal/features.py
+++ apollon/signal/features.py
@@ -5,6 +5,40 @@ from .. import segment as _segment
 from .. types import Array as _Array
 
 
+def spectral_centroid(inp: _Array, frqs: _Array) -> _Array:
+    """Estimate the spectral centroid frequency.
+
+    Calculation is applied to the second axis. One-dimensional
+    arrays will be promoted.
+
+    Args:
+        inp  (ndarray)    Input. Each row is assumend FFT bins scaled by `freqs`.
+        frqs (ndarray)    One-dimensional array of FFT frequencies.
+
+    Returns:
+        (ndarray or scalar)    Spectral centroid frequency.
+    """
+    inp = _np.atleast_2d(inp).astype('float64')
+
+    weighted_nrgy = _np.multiply(inp, frqs).sum(axis=1).squeeze()
+    total_nrgy = inp.sum(axis=1).squeeze()
+    total_nrgy[total_nrgy == 0.0] = 1.0
+
+    return _np.divide(weighted_nrgy, total_nrgy)
+
+def spectral_flux(inp: _Array) -> _Array:
+    """Estimate the spectral flux
+
+    Args:
+        inp (ndarray)    Input. Each row is assumend FFT bins.
+
+    Returns:
+        (ndarray)    Spectral flux.
+    """
+    inp = _np.atleast_2d(inp).astype('float64')
+    return _np.maximum(_np.diff(inp, axis=-1), 0).squeeze()
+
+
 def log_attack_time(inp: _Array, fs: int, ons_idx: _Array, wlen:float=0.05) -> _Array:
     """Estimate the attack time of each onset an return is logarithm.
 
