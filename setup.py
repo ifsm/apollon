32e44abbcf5cb57bcd207465f53d82914f5521be
Michael Blaß <michael.blass@uni-hamburg.de>
2019-03-21 11:42:31 +0100

    Renamed psycho_features -> roughness and adapted setup script.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from numpy.distutils.misc_util import get_numpy_include_dirs
 
 config = read_configuration('./setup.cfg')
 
-psycho_features = Extension('apollon.signal.psycho_features', sources=['apollon/signal/psycho_features_module.c'])
+psycho_features = Extension('apollon.signal.roughness', sources=['apollon/signal/roughness.c'])
 
 setup(include_dirs =  get_numpy_include_dirs(),
       ext_modules  = [psycho_features])
