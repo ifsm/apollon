087366c93dda2308d4aeb26c3d58786b5da8e0ca
Michael Blaß <michael.blass@uni-hamburg.de>
2019-04-28 09:05:31 +0200

    Refactoring.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#! python3
+#!/usr/bin/env  python3
 
 from setuptools import setup, Extension
 from setuptools.config import read_configuration
@@ -7,6 +7,7 @@ from numpy.distutils.misc_util import get_numpy_include_dirs
 
 config = read_configuration('./setup.cfg')
 
+# Extension modules
 psycho_features = Extension('apollon.signal.roughness', sources=['apollon/signal/roughness.c'])
 
 setup(include_dirs =  get_numpy_include_dirs(),
