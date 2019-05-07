65cce8cd5c330a340f6ae44bb8cabcd85acd4d3d
Michael Blaß <michael.blass@uni-hamburg.de>
2019-05-07 09:36:26 +0200

    Refactoring.

--- apollon/aplot.py
+++ apollon/aplot.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""
+aplot.py -- General plotting routines.
 
-"""aplot.py
-
-(c) Michael Blaß, 2016
-
-An implementation of advanced plotting utilities, that fits the common
-Apollon workflow.
+Copyright (C) 2018-2019 Michael Blaß
+michael.blass@uni-hamburg.de
 
 Functions:
     _new_figure         Create new (fig, ax) tuple.
@@ -17,40 +13,50 @@ Functions:
     signal              Plot a time domain signal.
 """
 
+from typing import Optional, Tuple
 
 import matplotlib.pyplot as _plt
 import matplotlib.cm as _cm
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as _np
 from scipy import stats as _stats
 
 from . import _defaults
+from . import tools as _tools
+from . types import Array as _Array
 
 
-def _new_figure(**kwargs):
-    """Return an empty figure."""
-    return _plt.figure(**kwargs)
+Limits = Optional[Tuple[int, int]]
+FigType = Optional[_plt.Figure]
+FigSize = Tuple[float, float]
+SubplotPos = Optional[Tuple[int, int, int]]
 
 
-def _new_axis(spines='nice', xlim=None, ylim=None, dp=(10, 10),
-              fig=None, sp_pos=(1,1,1), axison=True, **kwargs):
-    '''Create a new figure with a single axis and fancy spines.
+def _new_axis(spines: str = 'nice', xlim: Limits = None, ylim: Limits = None,
+              ticks_offset: Tuple[float, float] = (10, 10), fig: FigType = None,
+              sp_pos: SubplotPos = None, axison: bool = True, **kwargs) -> tuple:
+    """Create a new figure with a single axis and fancy spines.
 
-    Params:
-        spines    (str) Plot mode for spines. Eiterh 'nice' or 'standard'.
-        xlim      (tuple) Data extent on abscissae.
-        ylim      (tuple) Data extent on ordinate.
-        dp        (tuple) Distance of ticks in percet of data extent.
-        fig       (plt.figure) Existing figure.
-        sp_pos    (tuple) Position of the axis in the figure.
-        axison    (bool) Draw spines if True.
-        **kwargs  pass all keywords to matplotlib.figure.
+    All kwargs are passed on to _plt.figure().
 
-    Return:
-        (tuple)    A figure and a AxesSubplot instance.
-    '''
-    fig = _new_figure(**kwargs) if fig is None else fig
+    Args:
+        spines:          Plot mode for spines. Either 'nice' or 'standard'.
+        xlim:            Data extent on abscissae.
+        ylim:            Data extent on ordinate.
+        ticks_offset:    Distance of ticks in percet of data extent.
+        fig:             Existing figure.
+        sp_pos:          Position of the axis in the figure.
+        axison:          Draw spines if True.
 
+    Returns:
+        Figure and axes.
+    """
+
+    # pylint: disable=too-many-arguments
+
+    fig = _plt.figure(**kwargs) if fig is None else fig
+
+    if sp_pos is None:
+        sp_pos = (1, 1, 1)
     ax = fig.add_subplot(*sp_pos)
 
     if axison:
@@ -65,153 +71,179 @@ def _new_axis(spines='nice', xlim=None, ylim=None, dp=(10, 10),
 
             # Set ticks
             if xlim is not None:
-                d = (xlim[1] - xlim[0]) * dp[0] / 100
-                xt = _np.arange(xlim[0], xlim[1]+1, d)
-                ax.set_xticks(xt)
+                step_size = (xlim[1] - xlim[0]) * ticks_offset[0] / 100
+                ticks = _np.arange(xlim[0], xlim[1]+1, step_size)
+                ax.set_xticks(ticks)
 
             if ylim is not None:
-                d = (ylim[1] - ylim[0]) * dp[1] / 100
-                yt = _np.arange(ylim[0], ylim[1]+1, d)
-                ax.set_yticks(yt)
+                step_size = (ylim[1] - ylim[0]) * ticks_offset[1] / 100
+                ticks = _np.arange(ylim[0], ylim[1]+1, step_size)
+                ax.set_yticks(ticks)
 
             # Show ticks on left and bottom spines, only
             ax.xaxis.set_ticks_position('bottom')
             ax.yaxis.set_ticks_position('left')
-            return ax
 
-        elif spines == 'standard':
-            return ax
-        else:
-            raise ValueError('Unknown spine plot mode: {}'.format(spines))
-    else:
-        ax.axison=False
-        return ax
+        return fig, ax
+
+    ax.axison = False
+    return fig, ax
 
 
-def _new_axis_3d(fig=None, **kwargs):
-    '''Create a new figure with one single 3d axis.
+def _new_axis_3d(fig=None, **kwargs) -> tuple:
+    """Create a 3d cartesian coordinate system.
 
-    Params:
-        fig         (plt.figure) Existing figure.
-        ''kwargs    pass all keywords argumets to matplotlib.figure.
+    Args:
+        fig:    Place the new Axes3d object in ``fig``.
+                If fig is ``None``, a new figure is created.
 
-    Returns
-        (tuple)     plt.figure and plt.axes._subplots.Axes3DSubplot
-    '''
-    fig = _new_figure(**kwargs) if fig is None else fig
+    Returns:
+        Figure and axes.
+    """
+    fig = _plt.figure(**kwargs) if fig is None else fig
     ax_3d = fig.add_subplot(1, 1, 1, projection='3d')
     return fig, ax_3d
 
 
+def signal(values: _Array, fps: int, xaxis_scale: str = 'seconds', fig_size: FigSize = (10, 4),
+           **kwargs) -> tuple:
+    """Plot time series with constant sampling interval
 
-def fourplot(data, lag=1, standardized=True):
-    # preprocess data
-    if standardized is True:
-        data = (data - _np.mean(data)) / _np.std(data)
+    Args:
+        values:    Values of the time series.
+        fps:       Sampling frequency in samples.
 
-    # equivalent to _np.roll(data, 1) but faster
-    lag_data = _np.append(data[-lag:], data[:-lag])
+    Returns:
+        Figure and Axes.
+    """
+    fig, ax = _new_axis(fig_size=fig_size, **kwargs)
+
+    if xaxis_scale == 'seconds':
+        domain = _np.arange(0, values.size, dtype=float) / fps
+        ax.set_xlabel('Time [s]')
+    else:
+        domain = _np.arange(values.size)
+        ax.set_xlabel('Time [samples]')
+
+    ax.set_ylabel('Amplitude')
+    ax.plot(domain, values, **_defaults.PP_SIGNAL)
+
+    return fig, ax
+
+
+def fourplot(data: _Array, lag: int = 1) -> tuple:
+    """Plot time series, lag-plot, histogram, and probability plot.
+
+    Args:
+        data:    Input data set.
+        lag:     Lag for lag-plot given in number of samples.
+
+    Returns:
+        Parameters
+    """
+
+    # pylint: disable=invalid-name
+
+    data = _tools.standardize(data)
     (osm, osr), (slope, intercept, r) = _stats.probplot(data, dist='norm')
-    x = _np.arange(_np.ceil(osm[0])-1, _np.ceil(osm[-1]+1))
-    regr = slope*x+intercept
+    x_scale = _np.arange(_np.ceil(osm[0])-1, _np.ceil(osm[-1]+1))
+    regr = slope * x_scale + intercept
 
     # set up the figure
-    fig, ((ax1, ax2), (ax3, ax4)) = _plt.subplots(2, 2, figsize=(10, 6))
+    _, ((ax1, ax2), (ax3, ax4)) = _plt.subplots(2, 2, figsize=(10, 6))
     _plt.subplots_adjust(wspace=.3, hspace=.5)
 
     # data as time series
     ax1.plot(data, lw=2, alpha=.5)
-    ax1.set_title(r'Time series ($n$={})'.format(len(data)))
-    ax1.set_xlabel('Index i')
-    ax1.set_ylabel('Value')
+    ax1.set_title(r'Time series ($N$={})'.format(data.size))
+    ax1.set_xlabel('i')
+    ax1.set_ylabel(r'x[$i$]')
 
     # lag-plot
-    ax2.scatter(data, lag_data, alpha=.5)
-    ax2.plot(data, lag_data, 'k', alpha=.05)
-    ax2.set_title(r'Lag-plot ($\theta$={})'.format(lag))
-    ax2.set_xlabel('Index i-{}'.format(lag))
-    ax2.set_ylabel('Index i')
+    ax2.scatter(data[:-lag], data[lag:], alpha=.5)
+    ax2.plot(data[:-lag], data[lag:], 'k', alpha=.05)
+    ax2.set_title(r'Lag plot ($ \ \theta$={})'.format(lag))
+    ax2.set_xlabel(r'x[$i$]')
+    ax2.set_ylabel(r'x[$i-\theta$]')
 
     # histogram
-    counts, edges, patches = ax3.hist(data, alpha=.5, align='left')
-    ax3.set_xticks(edges[:-1])
-    ax3.set_xticklabels(edges[:-1])
-    ax3.set_title('Normalized histogram')
-    ax3.set_xlabel('Value')
-    ax3.set_ylabel('Frequency')
+    ax3.hist(data, alpha=.5, align='mid')
+    ax3.set_title('Histogram')
+    ax3.set_xlabel('bins')
+    ax3.set_ylabel('Number of samples per bin')
 
-    # probabiliti plot
+    # probability plot
     ax4.scatter(osm, osr, alpha=.5)
-    ax4.plot(x, regr, 'r', lw=2, alpha=.8)
+    ax4.plot(x_scale, regr, 'r', lw=2, alpha=.8)
     ax4.set_title(r'Normal probability plot ($R^2$={})'
                   .format((r**2).round(4)))
     ax4.set_xlabel('Qunatiles')
-    ax4.set_ylabel('Ordered values')
+    ax4.set_ylabel('Sorted values')
 
     return osm, osr, slope, intercept, r
 
 
+def marginal_distr(train_data: _Array, state_means: _Array, stat_dist: _Array, bins: int = 20,
+                   fig_size: Tuple[float, float] = (10, 4), legend: bool = True) -> tuple:
+    """Plot the marginal distribution of a PoissonHMM.
 
-def marginal_distr(x, lambda_, delta, figsize=(10, 4), bins=20, legend=True):
-    '''Plot the marginal distribution of a PoissonHMM.
-    Params:
-        x        (array-like) time series of HMM training data
-        lambda_  (array-like) componet distribution means
-        delta    (array-like) stationary distribution of Poisson HMM
+    Args:
+        train_data:     Training data set.
+        state_means:    State dependend means.
+        stat_dist:      Stationary distribution.
 
-    Return:
-        (fig, ax)   plot context.'''
-    ax = _new_axis(figsize=figsize)
-    n, bin_edges, patches = ax.hist(x, normed=True, alpha=.2, bins=bins)
+    Returns:
+        Figure and Axes.
+    """
 
-    for i, (me, de) in enumerate(zip(lambda_, delta)):
-        a, b = _stats.poisson.interval(.9999, me)
-        t = _np.arange(a, b, dtype=int)
-        y = _stats.poisson.pmf(t, me) * de
+    # pylint: disable=too-many-arguments, too-many-locals
 
-        ax.plot(t, y, alpha=.7, lw=2, ls='dashed', label='$\lambda_{}={}$'
-                .format(i, round(me, 2)))
+    _, ax = _new_axis(figsize=fig_size)
+    _ = ax.hist(train_data, normed=True, alpha=.2, bins=bins)
+
+    for i, (mean_val, stat_prob) in enumerate(zip(state_means, stat_dist)):
+        lower, upper = _stats.poisson.interval(.9999, mean_val)
+        support = _np.arange(lower, upper, dtype=int)
+        prob_mass = _stats.poisson.pmf(support, mean_val) * stat_prob
+
+        plot_label = r'$\lambda_{}={}$'.format(i, round(mean_val, 2))
+        _ = ax.plot(support, prob_mass, alpha=.7, lw=2, ls='dashed', label=plot_label)
 
     if legend:
         # Place legend outside the axe
-        ax.legend(bbox_to_anchor=(1.05, 1), loc=2)
+        _ = ax.legend(bbox_to_anchor=(1.05, 1), loc=2)
     return ax
 
 
+def onsets(odf: _Array, onset_index: _Array, fig_size: FigSize = (10, 5), **kwargs) -> None:
+    """Indicate onsets on a time series.
 
-def onsets(x, fs, onset_times, figsize=(10, 5), **kwargs):
-    ax = signal(x, fs, figsize=figsize, **kwargs)
-    ax.vlines(onset_times, -1, 1, **_defaults.PP_ONSETS)
-    return ax
+    Args:
+        odf:            Onset detection function or an arbitrary time series.
+        onset_index:    Onset indices relative to ``odf``.
+        fig_size:       Width and heigth of figure.
 
-
-
-def onset_decoding(sig, odx, dec):
-    '''Plot sig and and onsetes color coded regarding dec.'''
-    ax = signal(sig, params=_defaults.PP_SIG_ONS)
-    lc = max(dec) + 1
-    colors = _np.linspace(0, 1, lc)
-    ax.vlines(odx, -1, 1, linewidths=3, linestyle='dashed',
-              colors=_cm.viridis(colors[dec]))
-    return ax
-
-
-
-def signal(x, fs, xaxis_scale='seconds', figsize=(10, 4), params=None, **kwargs):
-    """Plot a signal on fancy axes.
+    Returns:
+        Figure and axes.
     """
-    ax = _new_axis(figsize=figsize)
+    fig, ax = signal(odf, fps=None, figsize=fig_size, **kwargs)
+    ax.vlines(onset_index, -1, 1, **_defaults.PP_ONSETS)
+    return fig, ax
 
-    if xaxis_scale == 'seconds':
-        t = _np.arange(0, x.size, dtype=float) / fs
-        ax.set_xlabel('Time [s]')
-    else:
-        t = _np.arange(x.size)
-        ax.set_xlabel('Samples')
 
-    ax.set_ylabel('Amplitude')
+def onset_decoding(odf: _Array, onset_index: _Array, decoding: _Array, cmap='viridis') -> tuple:
+    """Plot sig and and onsetes color coded regarding dec.
 
-    params = _defaults.PP_SIGNAL
-    ax.plot(t, x, **params)
+    Args:
+        odf:            Onset detection function or an arbitrary time series.
+        onset_index:    Onset indices relative to ``odf``.
+        decoding:       State codes in [0, ..., n].
 
-    return ax
+    Returns:
+        Figure and axes.
+    """
+    fig, ax = onsets(odf, onset_index)
+    color_space = getattr(_cm, cmap)(_np.linspace(0, 1, decoding.max()+1))
+    ax.vlines(onset_index, -1, 1, linewidths=3, linestyle='dashed',
+              colors=color_space(decoding))
+    return fig, ax
