be8ba8c0fd7c5edf5e3a2bcedfa4b41947fd6922
Michael Blaß <michael.blass@uni-hamburg.de>
2019-05-22 14:41:08 +0200

    Removed old license note.

--- apollon/hmm/poisson/poisson_hmm.py
+++ apollon/hmm/poisson/poisson_hmm.py
@@ -2,20 +2,6 @@
 poisson_hmm.py -- HMM with Poisson-distributed state dependent process.
 Copyright (C) 2018  Michael Blaß <michael.blass@uni-hamburg.de>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 Functions:
     to_txt                  Serializes model to text file.
     to_json                 JSON serialization.
