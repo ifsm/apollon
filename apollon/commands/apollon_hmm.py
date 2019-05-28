7c1465165d0fcf5d450f0e4002e01e693633ea4d
Michael Blaß <michael.blass@uni-hamburg.de>
2019-05-28 14:53:23 +0200

    apollon hmm clt now takes multiple track files.

--- apollon/commands/apollon_hmm.py
+++ apollon/commands/apollon_hmm.py
@@ -15,41 +15,38 @@ from .. import io
 from .. hmm import PoissonHmm
 from .. types import Array as _Array
 
-def main(argv=None) -> int:
-    if argv is None:
-        argv = sys.argv
 
-    track_file = pathlib.Path(argv.track_file)
-    if track_file.exists() and track_file.is_file():
-        with track_file.open('r') as fobj:
-            track_data = json.load(fobj, object_hook=io.decode_array)
+def _load_track_file(track_file: str) -> dict:
+    track_file = pathlib.Path(track_file)
+    with track_file.open('r') as fobj:
+        track_data = json.load(fobj, object_hook=io.decode_array)
+    return track_data
 
+
+def _parse_feature(track_data: dict, feature_path: str) -> _Array:
     feature = track_data
-    for key in argv.feature_path.split('.'):
+    for key in feature_path.split('.'):
         try:
             feature = feature[key]
         except KeyError:
             print('Error. Invalid node "{}" in feature path.'.format(key))
-            return 10
+            exit(10)
+    return feature
 
-    hmm = _train_n_hmm(feature, 3, 5)
-    if hmm is None:
-        return 10
 
-    default_fname = argv.feature_path.replace('.', '_') + '.hmm'
-    if argv.outpath is None:
+def _generate_outpath(in_path, out_path: str, feature_path: str) -> None:
+    in_path = pathlib.Path(in_path)
+    default_fname = '{}.hmm'.format(in_path.stem)
+    if out_path is None:
         out_path = pathlib.Path(default_fname)
     else:
-        out_path = pathlib.Path(argv.outpath)
+        out_path = pathlib.Path(out_path)
         if not out_path.suffix:
             out_path = out_path.joinpath(default_fname)
         if not out_path.parent.is_dir():
             print('Error. Path "{!s}" does not exist.'.format(out_path.parent))
-            return 10
-    print(out_path)
-
-    #io.dump_json(hmm.to_dict(), out_path)
-    return 0
+            exit(10)
+    return out_path
 
 
 def _train_n_hmm(data: _Array, m_states: int, n_trails: int):
@@ -73,9 +70,22 @@ def _train_n_hmm(data: _Array, m_states: int, n_trails: int):
 
     if len(trails) == 0:
         print('Error. Could not train HMM.')
-        return None
+        exit(10)
 
     return min(trails, key=lambda hmm: abs(hmm.quality.nll))
+
+
+def main(argv=None) -> int:
+    if argv is None:
+        argv = sys.argv
+
+    for trf in argv.track_files:
+        track_data = _load_track_file(trf)
+        feature = _parse_feature(track_data, argv.feature_path)
+        hmm = _train_n_hmm(feature, argv.mstates, 5)
+        out_path = _generate_outpath(trf, argv.outpath, argv.feature_path)
+        io.dump_json(hmm.to_dict(), out_path)
+    return 0
 
 
 if __name__ == '__main__':
