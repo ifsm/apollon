890f779b80f04cbf97982c8202a26f63fae0cf6f
Michael Blaß <michael.blass@uni-hamburg.de>
2019-06-06 13:51:25 +0200

    Mulitprocessing for apollon features.

--- apollon/commands/apollon_features.py
+++ apollon/commands/apollon_features.py
@@ -4,7 +4,9 @@
 
 
 import argparse
+import itertools
 import json
+import multiprocessing
 import sys
 import typing
 
@@ -18,21 +20,26 @@ def main(argv: dict = None) -> int:
     if argv is None:
         argv = sys.argv
 
-    for path in argv.files:
-        track_data = {}
-        if argv.rhythm:
-            track_data['rhythm'] = analyses.rhythm_track(path)
-
-        if argv.timbre:
-            track_data['timbre'] = analyses.timbre_track(path)
-
-        if argv.pitch:
-            track_data['pitch'] = analyses.pitch_track(path)
-
-        out_path = io.generate_outpath(path, argv.outpath, 'feat')
-        io.dump_json(track_data, out_path)
+    args = itertools.product(argv.files, [argv])
+    with multiprocessing.Pool(processes=24) as pool:
+        pool.starmap(_feature_extraction, args)
 
     return 0
+
+
+def _feature_extraction(path, args) -> None:
+    track_data = {}
+    if args.rhythm:
+        track_data['rhythm'] = analyses.rhythm_track(path)
+
+    if args.timbre:
+        track_data['timbre'] = analyses.timbre_track(path)
+
+    if args.pitch:
+        track_data['pitch'] = analyses.pitch_track(path)
+
+    out_path = io.generate_outpath(path, args.outpath, 'feat')
+    io.dump_json(track_data, out_path)
 
 
 if __name__ == '__main__':
