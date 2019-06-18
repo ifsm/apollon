07db1da1f5ef2ac381e5e8f7c4cee10760021478
Michael Blaß <michael.blass@uni-hamburg.de>
2019-06-18 17:25:46 +0200

    Removed apollon_export.

--- apollon/commands/__init__.py
+++ apollon/commands/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2019 Michael Blaß
 # michael.blass@uni-hamburg.de
 
-from . import apollon_export
 from . import apollon_features
 from . import apollon_onsets
 from . import apollon_hmm
