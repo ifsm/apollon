2c3f291d0b0925368a59ac9342d0c8d0ea0bfb83
Michael Blaß <michael.blass@uni-hamburg.de>
2019-11-19 11:57:38 +0100

    Updated tests regarding last commit for 	- signal.tools 	- signal.features

--- tests/test_signal_tools.py
+++ tests/test_signal_tools.py
@@ -4,29 +4,22 @@ import unittest
 import numpy as np
 
 from apollon._defaults import SPL_REF
+from apollon.signal import features as feat
 from apollon.signal import tools as st
 
 
-class TestSpectral(unittest.TestCase):
+class TestAmp(unittest.TestCase):
     def setUp(self):
         self.lower_bound = np.array([SPL_REF, SPL_REF*0.1])
         self.range = np.array([SPL_REF+1e-6, 1.0])
 
-    def test_spl_lower_bound(self):
-        cnd = np.all(st.spl(self.lower_bound) == 0)
-        self.assertTrue(cnd)
-
-    def test_spl_range(self):
-        cnd = np.all(st.spl(self.range) > 0)
-        self.assertTrue(cnd)
-
     def test_amp_lower_bound(self):
-        res = st.amp(st.spl(self.lower_bound))
+        res = st.amp(feat.spl(self.lower_bound))
         cnd = np.array_equal(res, np.array([SPL_REF, SPL_REF]))
         self.assertTrue(cnd)
 
     def test_amp_range(self):
-        res = st.amp(st.spl(self.range))
+        res = st.amp(feat.spl(self.range))
         cnd = np.allclose(res, self.range)
         self.assertTrue(cnd)
 
