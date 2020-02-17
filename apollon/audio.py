0b3356615ff72ec7e7099efb17fb16e4a8d6835e
Michael Blaß <michael.blass@uni-hamburg.de>
2020-02-17 07:09:55 +0100

    Update audio module.

--- apollon/audio.py
+++ apollon/audio.py
@@ -1,9 +1,7 @@
-# Licensed under the terms of the BSD-3-Clause license.
-# Copyright (C) 2019 Michael Blaß
-# mblass@posteo.net
+"""apollon/audio.py -- Wrapper classes for audio data.
 
-"""
-apollon/audio.py -- Wrapper classes for audio data.
+Licensed under the terms of the BSD-3-Clause license.
+Copyright (C) 2019 Michael Blaß, mblass@posteo.net
 
 Classes:
     AudioFile   Representation of an audio file.
@@ -12,7 +10,9 @@ Functions:
     fti16        Cast float to int16.
     load_audio   Load .wav file.
 """
+from dataclasses import dataclass
 import pathlib as _pathlib
+from typing import Generator
 
 import matplotlib.pyplot as _plt
 import numpy as np
@@ -30,29 +30,47 @@ class AudioFile:
             norm:   If True, signal will be normalized ]-1, 1[.
             mono:   If True, mixdown all channels.
     """
-    def __init__(self, path: PathType, norm: bool = False, mono: bool = True) -> None:
+    def __init__(self, path: PathType, norm: bool = False,
+                 mono: bool = True) -> None:
         """Load an audio file."""
 
-        self.file = _pathlib.Path(path)
-        self.data, self.fps = _sf.read(self.file, dtype='float')
-        self.size = self.data.shape[0]
+        self._path = _pathlib.Path(path)
+        self._file = _sf.SoundFile(self.path)
+        self.norm = norm
+        self.mono = mono
 
-        if mono and self.data.ndim > 1:
-            self.data = self.data.sum(axis=1) / self.data.shape[1]
+    @property
+    def data(self) -> np.ndarray:
+        data = self._file.read(always_2d=True)
+        if self.mono and self.n_channels > 1:
+            data = data.sum(axis=1, keepdims=True) / self.n_channels
+        if self.norm:
+            data = _ast.normalize(data)
+        self._file.seek(0)
+        return data
 
-        if norm:
-            self.data = _ast.normalize(self.data)
+    @property
+    def n_channels(self) -> int:
+        return self._file.channels
 
-    def cut(self, start, n_samples):
-        """Cut a segment of length ``n_samples`` form data, beginning
-        at ``start``.
+    @property
+    def n_frames(self) -> int:
+        return self._file.frames
 
-        Args:
-            start:      Sample index to start.
-            n_samples:  Number of samples to cut.
-        """
-        self.data = self.data[start:start+n_samples]
-        self.size = self.data.shape[0]
+    @property
+    def fps(self) -> int:
+        return self._file.samplerate
+
+    @property
+    def path(self) -> str:
+        return str(self._path)
+
+    @property
+    def shape(self) -> tuple:
+        return self.n_frames, self.n_channels
+
+    def close(self) -> None:
+        self._file.close()
 
     def plot(self) -> None:
         """Plot audio as wave form."""
@@ -62,16 +80,13 @@ class AudioFile:
 
     def __str__(self):
         return "<{}, {} kHz, {:.3} s>" \
-        .format(self.file.name, self.fps/1000, self.size/self.fps)
+               .format(self._path.name, self.fps/1000, self.n_frames/self.fps)
 
     def __repr__(self):
         return self.__str__()
 
     def __len__(self):
         return self.size
-
-    def __getitem__(self, item):
-        return self.data[item]
 
 
 def fti16(inp: Array) -> Array:
@@ -86,7 +101,8 @@ def fti16(inp: Array) -> Array:
     return np.clip(np.floor(inp*2**15), -2**15, 2**15-1).astype('int16')
 
 
-def load_audio(path: PathType, norm: bool = False, mono: bool = True) -> AudioFile:
+def load_audio(path: PathType, norm: bool = False, mono: bool = True
+          ) -> AudioFile:
     """Load an audio file.
 
     Args:
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,26 +1,80 @@
-#!/usr/bin/env python3
-
 import unittest
 import numpy as np
 
-from apollon.audio import load_audio, fti16
-from apollon.types import Array
+from apollon.audio import AudioFile
 
 
-class Test_ModulAudio(unittest.TestCase):
+class TestAudioFileMono(unittest.TestCase):
     def setUp(self):
-        self.snd = load_audio('audio/beat.wav')
+        self.snd_mono = AudioFile('audio/beat.wav', norm=False, mono=True)
+        self.snd_multi = AudioFile('audio/beat_5ch.wav', norm=False, mono=True)
+        self.comp_mono = self.snd_mono._file.read(always_2d=True)
+        self.snd_mono._file.seek(0)
 
-    def test_AudioData(self):
-        self.assertTrue(isinstance(self.snd.fps, int))
-        self.assertTrue(isinstance(self.snd.data, Array))
+    def test_data_property(self):
+        print(self.snd_multi.data.shape, self.comp_mono.shape)
+        self.assertTrue(np.array_equal(self.snd_mono.data, self.comp_mono))
+        self.assertTrue(np.array_equal(self.snd_multi.data, self.comp_mono))
 
-    def test_fti16(self):
-        res = fti16(self.snd.data)
-        self.assertTrue(isinstance(res, Array))
-        self.assertTrue(res.dtype == 'int16')
-        self.assertTrue(self.snd.data.shape == res.shape)
+    def tearDown(self):
+        self.snd_mono.close()
 
+
+class TestAudioFileMultiChannel(unittest.TestCase):
+    def setUp(self):
+        self.snd_mono = AudioFile('audio/beat.wav', mono=False)
+        self.snd_multi = AudioFile('audio/beat_5ch.wav', mono=False)
+        self.comp_mono = self.snd_mono._file.read(always_2d=True)
+        self.comp_multi = self.snd_multi._file.read(always_2d=True)
+        self.snd_mono._file.seek(0)
+        self.snd_multi._file.seek(0)
+
+    def test_data_property(self):
+        self.assertTrue(np.array_equal(self.snd_mono.data, self.comp_mono))
+        self.assertTrue(np.array_equal(self.snd_multi.data, self.comp_multi))
+
+    def tearDown(self):
+        self.snd_mono.close()
+        self.snd_multi.close()
+
+
+class TestAudioFileMonoNormalize(unittest.TestCase):
+    def setUp(self):
+        self.snd_mono = AudioFile('audio/beat.wav', mono=True, norm=True)
+        self.snd_multi = AudioFile('audio/beat_5ch.wav', mono=True, norm=True)
+
+    def test_data_property(self):
+        self.assertTrue(np.less_equal(self.snd_mono.data, 1.0).all())
+        self.assertTrue(np.greater_equal(self.snd_mono.data, -1.0).all())
+        self.assertTrue(np.less_equal(self.snd_multi.data, 1.0).all())
+        self.assertTrue(np.greater_equal(self.snd_multi.data, -1.0).all())
+
+    def tearDown(self):
+        self.snd_mono.close()
+        self.snd_multi.close()
+
+
+class TestAudioFileMultiChNormalize(unittest.TestCase):
+    def setUp(self):
+        self.snd_mono = AudioFile('audio/beat.wav', mono=False, norm=True)
+        self.snd_multi = AudioFile('audio/beat_5ch.wav', mono=False, norm=True)
+
+    def test_data_property(self):
+        self.assertTrue(np.less_equal(self.snd_mono.data, 1.0).all())
+        self.assertTrue(np.greater_equal(self.snd_mono.data, -1.0).all())
+        self.assertTrue(np.less_equal(self.snd_multi.data, 1.0).all())
+        self.assertTrue(np.greater_equal(self.snd_multi.data, -1.0).all())
+
+    def tearDown(self):
+        self.snd_mono.close()
+        self.snd_multi.close()
+"""
+def test_fti16(self):
+    res = fti16(self.snd_mono.data)
+    self.assertTrue(isinstance(res, Array))
+    self.assertTrue(res.dtype == 'int16')
+    self.assertTrue(self.snd_mono.data.shape == res.shape)
+"""
 
 if __name__ == '__main__':
     unittest.main()
