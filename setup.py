8052d81651f56d30b91104975721b93b6127c129
Michael Blaß <michael.blass@uni-hamburg.de>
2020-06-03 15:37:44 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 from setuptools import setup, Extension
 from setuptools.config import read_configuration
-
-
-class GetNumpyInclude:
-    """Postpone the numpy import.
-    This enables the package to be installed with setuptools,
-    and to build numpy based extension modules during installation.
-    See https://stackoverflow.com/questions/54117786/
-    add-numpy-get-include-argument-to-setuptools-without-preinstalled-numpy.
-    """
-    def __str__(self):
-        import numpy
-        return numpy.get_include()
+import numpy as np
 
 
 config = read_configuration('./setup.cfg')
 
 ext_features = Extension('_features',
-    sources = ['apollon/signal/cdim.c',
-               'apollon/signal/correlogram.c',
-               'apollon/signal/_features_module.c'],
-    include_dirs = ['include', GetNumpyInclude()])
+    sources = ['src/apollon/signal/cdim.c',
+               'src/apollon/signal/correlogram.c',
+               'src/apollon/signal/_features_module.c'],
+    include_dirs = ['include', np.get_include()])
 
 
 setup(ext_modules = [ext_features])
+
