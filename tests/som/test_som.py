999198c7ef19be1d29ae9b72830c3600ea2e0c99
Michael Blaß <michael.blass@uni-hamburg.de>
2020-07-24 10:00:20 +0200

    Updated som tests.

--- tests/som/test_som.py
+++ tests/som/test_som.py
@@ -1,13 +1,74 @@
-#!/usr/bin/python3
-
-
 import unittest
+from typing import Tuple
+
+from hypothesis import given
+import hypothesis.strategies as hst
 import numpy as np
 import scipy as sp
 
-from apollon.som.som import SelfOrganizingMap
+from apollon.som.som import SomBase, SomGrid
+
+SomDim = Tuple[int, int, int]
+dimension = hst.integers(min_value=1, max_value=100)
+som_dims = hst.tuples(dimension, dimension, dimension)
 
 
+class TestSomBase(unittest.TestCase):
+    @given(som_dims)
+    def test_dims(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertEqual(som.dims, dims)
+
+    @given(som_dims)
+    def test_dx(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertEqual(som.dx, dims[0])
+
+    @given(som_dims)
+    def test_dy(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertEqual(som.dy, dims[1])
+
+    @given(som_dims)
+    def test_dw(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertEqual(som.dw, dims[2])
+
+    @given(som_dims)
+    def test_n_units(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertEqual(som.n_units, dims[0]*dims[1])
+
+    @given(som_dims)
+    def test_shape(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertEqual(som.shape, (dims[0], dims[1]))
+
+    @given(som_dims)
+    def test_grid(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertIsInstance(som.grid, SomGrid)
+
+    """
+    @given(som_dims)
+    def test_dists(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertIsInstance(som.dists, np.ndarray)
+    """
+
+    @given(som_dims)
+    def test_weights(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        self.assertIsInstance(som.weights, np.ndarray)
+
+    @given(som_dims)
+    def test_match(self, dims: SomDim) -> None:
+        som = SomBase(dims, 100, 0.1, 10, 'gaussian', 'uniform', 'euclidean')
+        data = np.random.rand(100, dims[2])
+        self.assertIsInstance(som.match(data), np.ndarray)
+
+
+"""
 class TestSelfOrganizingMap(unittest.TestCase):
     def setUp(self):
         N = 100
@@ -22,10 +83,7 @@ class TestSelfOrganizingMap(unittest.TestCase):
 
         self.data = np.vstack((seg1, seg2))
         self.dims = (10, 10, 2)
-
-    def test_init_random(self):
-        som = SelfOrganizingMap(self.dims, init_distr='uniform')
-        self.assertTrue('weights', True)
+"""
 
 if __name__ == '__main__':
     unittest.main()
--- tests/som/test_som_utilities.py
+++ tests/som/test_som_utilities.py
@@ -1,12 +1,45 @@
-#!/usr/bin/python3
-
 import unittest
+
+from hypothesis import strategies as hst
 import numpy as np
+from scipy.spatial import distance
 import scipy as sp
 
-from apollon.som import utilities
+from apollon.som import utilities as asu
+
+"""
+class TestMatch(unittest.TestCase):
+    def setUp(self) -> None:
+        self.weights = np.random.rand(100, 5)
+        self.data = np.random.rand(200, 5)
+
+    def test_returns_tuple(self) -> None:
+        res = asu.match(self.weights, self.data, 2, 'euclidean')
+        self.assertIsInstance(res, tuple)
+
+    def test_elements_are_arrays(self) -> None:
+        bmu, err = asu.match(self.weights, self.data, 'euclidean')
+        self.assertIsInstance(bmu, np.ndarray)
+        self.assertIsInstance(err, np.ndarray)
+
+    def test_correct_ordering(self) -> None:
+        kth = 5
+        bmu, err = asu.match(self.weights, self.data, 'euclidean')
+        wdists = distance.cdist(self.weights, self.data)
+        kswd = wdists.sort(axis=0)[:kth, :]
+"""
+
+class TestDistribute(unittest.TestCase):
+    def setUp(self) -> None:
+        self.n_units = 400
+        self.bmu = np.random.randint(0, self.n_units, 100)
+
+    def returns_dict(self):
+        res = asu.distribute(self.bmu, self.n_units)
+        self.assertIsInstance(res, dict)
 
 
+"""
 class TestSelfOrganizingMap(unittest.TestCase):
     def setUp(self):
         self.weights = np.load('data/test_weights.npy')
@@ -18,7 +51,7 @@ class TestSelfOrganizingMap(unittest.TestCase):
         bmu, err = utilities.best_match(self.weights, self.inp, 'euclidean')
         self.assertTrue(np.array_equiv(test_bmu, bmu))
         self.assertTrue(np.array_equiv(test_err, err))
-
+"""
 
 if __name__ == '__main__':
     unittest.main()
