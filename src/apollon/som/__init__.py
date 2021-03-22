26c5cabd25e6b14fa78281f9f5866031e3f78b74
Michael Blaß <michael.blass@uni-hamburg.de>
2021-03-22 10:58:11 +0100

    cleaned __init__.py

--- src/apollon/som/__init__.py
+++ src/apollon/som/__init__.py
@@ -1,11 +1,4 @@
-# Licensed under the terms of the BSD-3-Clause license.
-# Copyright (C) 2019 Michael Blaß
-# mblass@posteo.net
-
 """
-====================================
-Self-organizing map tools and stuff
-====================================
-
-The Self-organizing map [Kohonen1993] is a ... bla bla bla
+:copyright: 2019, Michael Blaß
+:license: BSD 3 Clause
 """
