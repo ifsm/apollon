37729868396e9a508a022c1de5c55350c883cb7a
Michael Blaß <mblass@posteo.de>
2022-12-18 21:34:34 +0100

    build: update dependencies
    - add hypothesis to dev deps

---
+++ build.py
@@ -0,0 +1,19 @@
+from setuptools.extension import Extension
+import numpy as np
+
+
+ext_features = Extension('_features',
+    sources = ['src/apollon/signal/cdim.c',
+               'src/apollon/signal/correlogram.c',
+               'src/apollon/signal/_features_module.c'],
+    include_dirs = ['include', np.get_include()])
+
+ext_som_dist = Extension('_distance',
+        sources = ['src/apollon/som/distance.c',
+                   'src/apollon/som/_distance_module.c'],
+        include_dirs = ['include', np.get_include()])
+
+
+def build(setup_kwargs):
+
+    setup_kwargs.update({"ext_modules": [ext_features, ext_som_dist] })
