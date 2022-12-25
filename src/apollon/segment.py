a067b5c010a95f7ff5f7ee20c204c70d01599da4
Michael Blaß <mblass@posteo.de>
2022-12-25 15:10:09 +0100

    refactor: variable name

--- src/apollon/segment.py
+++ src/apollon/segment.py
@@ -297,14 +297,14 @@ class LazySegments:
             yield self.compute_bounds(i)
 
 
-def _by_samples(x: Array, n_perseg: int) -> Array:
-    """Split ``x`` into segments of lenght ``n_perseg`` samples.
+def _by_samples(arr: Array, n_perseg: int) -> Array:
+    """Split ``arr`` into segments of lenght ``n_perseg`` samples.
 
     This function automatically applies zero padding for inputs that cannot be
     split evenly.
 
     Args:
-        x:         One-dimensional input array.
+        arr:       One-dimensional input array.
         n_perseg:  Length of segments in samples.
 
     Returns:
@@ -317,21 +317,21 @@ def _by_samples(x: Array, n_perseg: int) -> Array:
         raise ValueError('``n_perchunk`` out of range. '
                          'Expected 1 <= n_perchunk.')
 
-    fit_size = int(_np.ceil(x.size / n_perseg) * n_perseg)
-    n_ext = fit_size - x.size
-    x = _zero_padding(x, n_ext)
-    return x.reshape(-1, n_perseg)
+    fit_size = int(_np.ceil(arr.size / n_perseg) * n_perseg)
+    n_ext = fit_size - arr.size
+    arr = _zero_padding(arr, n_ext)
+    return arr.reshape(-1, n_perseg)
 
 
-def _by_samples_with_hop(x: Array, n_perseg: int, hop_size: int) -> Array:
-    """Split `x` into segments of lenght `n_perseg` samples. Move the
+def _by_samples_with_hop(arr: Array, n_perseg: int, hop_size: int) -> Array:
+    """Split `arr` into segments of lenght `n_perseg` samples. Move the
     extraction window `hop_size` samples.
 
     This function automatically applies zero padding for inputs that cannot be
     split evenly.
 
     Args:
-        x:         One-dimensional input array.
+        arr:       One-dimensional input array.
         n_perseg:  Length of segments in samples.
         hop_size:  Hop size in samples
 
@@ -341,31 +341,31 @@ def _by_samples_with_hop(x: Array, n_perseg: int, hop_size: int) -> Array:
     if not (isinstance(n_perseg, int) and isinstance(hop_size, int)):
         raise TypeError('Params must be of type int.')
 
-    if not 1 < n_perseg <= x.size:
+    if not 1 < n_perseg <= arr.size:
         raise ValueError('n_perseg out of range. '
-                         'Expected 1 < n_perseg <= len(x).')
+                         'Expected 1 < n_perseg <= len(arr).')
 
     if hop_size < 1:
         raise ValueError('hop_size out of range. Expected 1 < hop_size.')
 
-    n_hops = (x.size - n_perseg) // hop_size + 1
+    n_hops = (arr.size - n_perseg) // hop_size + 1
     n_segs = n_hops
 
-    if (x.size - n_perseg) % hop_size != 0 and n_perseg > hop_size:
+    if (arr.size - n_perseg) % hop_size != 0 and n_perseg > hop_size:
         n_segs += 1
 
     fit_size = hop_size * n_hops + n_perseg
-    n_ext = fit_size - x.size
-    x = _zero_padding(x, n_ext)
+    n_ext = fit_size - arr.size
+    arr = _zero_padding(arr, n_ext)
 
-    out = _np.empty((n_segs, n_perseg), dtype=x.dtype)
+    out = _np.empty((n_segs, n_perseg), dtype=arr.dtype)
     for i in range(n_segs):
         off = i * hop_size
-        out[i] = x[off:off+n_perseg]
+        out[i] = arr[off:off+n_perseg]
     return out
 
 
-def by_samples(x: Array, n_perseg: int, hop_size: int = 0) -> Array:
+def by_samples(arr: Array, n_perseg: int, hop_size: int = 0) -> Array:
     """Segment the input into n segments of length n_perseg and move the
     window `hop_size` samples.
 
@@ -377,7 +377,7 @@ def by_samples(x: Array, n_perseg: int, hop_size: int = 0) -> Array:
     Overlap in percent is calculated as ov = hop_size / n_perseg * 100.
 
     Args:
-        x           One-dimensional input array.
+        arr         One-dimensional input array.
         n_perseg    Length of segments in samples.
         hop_size    Hop size in samples. If < 1, hop_size = n_perseg.
 
@@ -385,12 +385,12 @@ def by_samples(x: Array, n_perseg: int, hop_size: int = 0) -> Array:
         Two-dimensional array of segments.
     """
     if hop_size < 1:
-        return _by_samples(x, n_perseg)
+        return _by_samples(arr, n_perseg)
     else:
-        return _by_samples_with_hop(x, n_perseg, hop_size)
+        return _by_samples_with_hop(arr, n_perseg, hop_size)
 
 
-def by_ms(x: Array, fps: int, ms_perseg: int, hop_size: int = 0) -> Array:
+def by_ms(arr: Array, fps: int, ms_perseg: int, hop_size: int = 0) -> Array:
     """Segment the input into n segments of length ms_perseg and move the
     window `hop_size` milliseconds.
 
@@ -402,7 +402,7 @@ def by_ms(x: Array, fps: int, ms_perseg: int, hop_size: int = 0) -> Array:
     Overlap in percent is calculated as ov = hop_size / n_perseg * 100.
 
     Args:
-        x           One-dimensional input array.
+        arr         One-dimensional input array.
         fs          Sampling frequency.
         n_perseg    Length of segments in milliseconds.
         hop_size    Hop size in milliseconds. If < 1, hop_size = n_perseg.
@@ -412,17 +412,17 @@ def by_ms(x: Array, fps: int, ms_perseg: int, hop_size: int = 0) -> Array:
         """
     n_perseg = fps * ms_perseg // 1000
     hop_size = fps * hop_size // 1000
-    return by_samples(x, n_perseg, hop_size)
+    return by_samples(arr, n_perseg, hop_size)
 
 
-def by_onsets(x: Array, n_perseg: int, ons_idx: Array, off: int = 0
+def by_onsets(arr: Array, n_perseg: int, ons_idx: Array, off: int = 0
               ) -> Array:
-    """Split input `x` into len(ons_idx) segments of length `n_perseg`.
+    """Split input `arr` into len(ons_idx) segments of length `n_perseg`.
 
     Extraction windos start at `ons_idx[i]` + `off`.
 
     Args:
-        x           One-dimensional input array.
+        arr         One-dimensional input array.
         n_perseg    Length of segments in samples.
         ons_idx     One-dimensional array of onset positions.
         off         Length of offset.
@@ -431,18 +431,18 @@ def by_onsets(x: Array, n_perseg: int, ons_idx: Array, off: int = 0
         Two-dimensional array of shape (len(ons_idx), n_perseg).
     """
     n_ons = ons_idx.size
-    out = _np.empty((n_ons, n_perseg), dtype=x.dtype)
+    out = _np.empty((n_ons, n_perseg), dtype=arr.dtype)
 
     for i, idx in enumerate(ons_idx):
         pos = idx + off
         if pos < 0:
             pos = 0
-        elif pos >= x.size:
-            pos = x.size - 1
+        elif pos >= arr.size:
+            pos = arr.size - 1
 
-        if pos + n_perseg >= x.size:
-            buff = x[pos:]
+        if pos + n_perseg >= arr.size:
+            buff = arr[pos:]
             out[i] = _zero_padding(buff, n_perseg-buff.size)
         else:
-            out[i] = x[pos:pos+n_perseg]
+            out[i] = arr[pos:pos+n_perseg]
     return out
