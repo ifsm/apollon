83227952e0a4397cccf4c8b6f42564ca61d32e94
Michael Blaß <mblass@posteo.de>
2023-06-02 22:11:40 +0200

    build: update build extensions

--- build.py
+++ build.py
@@ -2,13 +2,13 @@ from setuptools.extension import Extension
 import numpy as np
 
 
-ext_features = Extension('_features',
+ext_features = Extension('apollon.signal._features',
     sources = ['src/apollon/signal/cdim.c',
                'src/apollon/signal/correlogram.c',
                'src/apollon/signal/_features_module.c'],
     include_dirs = ['include', np.get_include()])
 
-ext_som_dist = Extension('_distance',
+ext_som_dist = Extension('apollon.som._distance',
         sources = ['src/apollon/som/distance.c',
                    'src/apollon/som/_distance_module.c'],
         include_dirs = ['include', np.get_include()])
