aea0d3e25e19a85930287e902a54ea120dbe8fe1
Michael Blaß <mblass@posteo.net>
2023-06-29 11:07:16 +0200

    refactor: warning in lorentz system test

--- tests/test_fractal.py
+++ tests/test_fractal.py
@@ -1,17 +1,25 @@
+# pylint: disable = missing-function-docstring,missing-class-docstring
+from functools import partial
 import unittest
 
 from hypothesis import given
 from hypothesis.strategies import floats, integers, tuples, SearchStrategy
+import numpy as np
 from apollon import fractal
 
 Params = tuple[float, float, float]
 States = tuple[float, float, float]
 
+rfloats = partial(floats, allow_nan=False, allow_infinity=False)
+unit_floats = rfloats(min_value=0.0, max_value=1.0)
+
 def states() -> SearchStrategy:
-    return tuples(floats(), floats(), floats())
+    return tuples(unit_floats, unit_floats, unit_floats)
 
 def params() -> SearchStrategy:
-    return tuples(floats(), floats(), floats())
+    return tuples(rfloats(min_value=8.0, max_value=12.0),
+                  rfloats(min_value=26.0, max_value=30.0),
+                  rfloats(min_value=8/3-0.5, max_value=8/3+0.5))
 
 class TestLorenzSystem(unittest.TestCase):
     @given(states(), params())
@@ -21,8 +29,9 @@ class TestLorenzSystem(unittest.TestCase):
 
 
 class TestLorenzAttractor(unittest.TestCase):
-    @given(integers(min_value=1, max_value=5000), params(), states(),
-           floats(min_value=0.01, max_value=10))
+    @given(integers(min_value=1, max_value=1000), params(), states())
     def test_lorenz_attractor(self, n_samples: int, params: Params,
-                              init: States, diff: float) -> None:
-        fractal.lorenz_attractor(n_samples, *params, init, diff)
+                              init: States) -> None:
+        res = fractal.lorenz_attractor(n_samples, *params, init, 0.01)
+        self.assertFalse(np.isnan(res).any())
+        self.assertFalse(np.isinf(res).any())
