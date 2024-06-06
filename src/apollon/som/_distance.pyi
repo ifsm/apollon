1cb806deb05584f489f00a76d1bdd557cd1ddc86
Michael Blaß <m.blass@uke.de>
2024-06-06 09:54:58 +0200

    refactor: add missing types for _distance module

---
+++ src/apollon/som/_distance.pyi
@@ -0,0 +1,10 @@
+from numbers import Real
+from typing import Sequence
+from apollon.types import FloatArray
+
+
+def hellinger(prob_a: Sequence[Real], prob_b: Sequence[Real]) -> float:
+    ...
+
+def hellinger_stm(sm_A: FloatArray, sm_B: FloatArray) -> float:
+    ...
