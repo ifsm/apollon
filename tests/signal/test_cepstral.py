7ad351c355a662032f1b7ea9505870a0c5d84854
Michael Blaß <m.blass@uke.de>
2024-07-11 09:55:36 +0200

    test: add basic test

--- tests/signal/test_cepstral.py
+++ tests/signal/test_cepstral.py
@@ -5,19 +5,30 @@ from hypothesis import strategies as st
 from hypothesis import assume, given
 
 from apollon.signal.cepstral import Mfcc
+from apollon.signal.spectral import Dft
+from apollon.signal.filter import TriangFilterSpec
 
+from .strategies import frequencies, samplerates, fftsizes
 
-frqs = st.floats(min_value=0, max_value=10000, allow_infinity=False, allow_nan=False)
 filts = st.integers(min_value=1, max_value=100)
-samplerates = st.integers(min_value=1, max_value=96000)
-fftsizes = st.integers(min_value=1, max_value=48000)
 
 
 class TestMfcc(TestCase):
-    @given(samplerates, fftsizes, frqs, frqs, filts)
-    def test__mfcc(self, fps: int, n_fft: int, low: float, high: float, n_filters: int) -> None:
-        assume(low<high)
-        Mfcc(fps, n_fft, low, high, n_filters)
-    
-    #def test__mfcc_from_stft(self)
-    #def test__mfcc_from_dft(self)
+    def setUp(self) -> None:
+        self.fps = 44100
+        self.n_fft = 44100
+        self.lcf = 80
+        self.ucf = 5000
+        self.n_filters = 50
+        self.window = "hamming"
+
+    def test__mfcc(self) -> None:
+        Mfcc(self.fps, self.n_fft, self.lcf, self.ucf, self.n_filters)
+
+    def test__mfcc_from_stft(self) -> None:
+        dft = Dft(self.fps, self.window, self.n_fft)
+        spec = TriangFilterSpec(low=self.lcf, high=self.ucf, n_filters=self.n_filters)
+        mfcc = Mfcc.from_dft(dft, spec)
+        self.assertIsInstance(mfcc, Mfcc)
+        self.assertEqual(mfcc.fps, dft.params.fps)
+        self.assertEqual(mfcc.n_fft, dft.params.n_fft)
